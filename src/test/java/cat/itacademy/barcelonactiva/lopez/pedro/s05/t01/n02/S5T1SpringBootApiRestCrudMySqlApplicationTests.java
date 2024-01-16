4dc8eff9bce11b9d912b3f02562ec6c1aab6ef4a
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-16 11:22:05 +0100

    creates S5T1SpringBootApiRestCrudMySqlApplicationTests

---
+++ src/test/java/cat/itacademy/barcelonactiva/lopez/pedro/s05/t01/n02/S5T1SpringBootApiRestCrudMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.lopez.pedro.s05.t01.n02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5T1SpringBootApiRestCrudMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
